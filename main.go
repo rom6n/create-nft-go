ac085e6598454bf3719bf8b066ad00be611f1892
Roman <roman.mainn@gmail.com>
2025-08-18 14:24:51 +0500

    added CORS 2

--- main.go
+++ main.go
@@ -209,7 +209,7 @@ func main() {
 	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "18.156.158.53,18.156.42.200,52.59.103.54",
+		AllowOrigins: "*",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
