34e67403fcb75ca33625bb4d8d72372f73ebcd75
Roman <roman.mainn@gmail.com>
2025-08-25 02:04:51 +0500

    .

--- main.go
+++ main.go
@@ -209,7 +209,7 @@ func main() {
 	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://rom6n.github.io",
+		AllowOrigins: "*",//"https://rom6n.github.io",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
